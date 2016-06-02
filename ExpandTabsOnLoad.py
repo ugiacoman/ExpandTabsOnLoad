3a7d7943c63cdade01bb46415722f0a5348a32b5
Ulises Giacoman <ugiacoman@email.wm.edu>
2016-06-02 16:00:30 -0400

    Update ExpandTabsOnLoad.py
    Fixed for ST3, will convert all tabs to spaces on file load.

--- ExpandTabsOnLoad.py
+++ ExpandTabsOnLoad.py
@@ -5,12 +5,5 @@ import sublime_plugin
 
 
 class ExpandTabsOnLoad(sublime_plugin.EventListener):
-    # Run ST's 'expand_tabs' command when opening a file,
-    # only if there are any tab characters in the file
     def on_load(self, view):
-        expand_tabs = view.settings().get("expand_tabs_on_load", False)
-        if expand_tabs and view.find("\t", 0):
-            view.run_command("expand_tabs")
-            tab_size = view.settings().get("tab_size", 0)
-            message = "Converted tab characters to {0} spaces".format(tab_size)
-            sublime.status_message(message)
+        view.run_command('expand_tabs', {"set_translate_tabs": True})
